5fc91947d012f9f7b44443a92ee67367f811abc2
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2025-05-03 14:10:22 -0700

    Add example threejs project

--- scripts/threejs/tutorial.js
+++ scripts/threejs/tutorial.js
@@ -6,7 +6,9 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+
+const threeCanvas = document.querySelector("#threejs-canvas");
+threeCanvas.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -17,9 +19,9 @@ camera.position.z = 5;
 
 function animate() {
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
-  renderer.render( scene, camera );
+	renderer.render( scene, camera );
 
 }
