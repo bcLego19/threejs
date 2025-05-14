afca542578f05404ea4643f8766d29839ea1ca9e
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2025-05-14 12:39:07 -0700

    Update 3d-room.js

--- scripts/threejs/3d-room.js
+++ scripts/threejs/3d-room.js
@@ -5,7 +5,7 @@ import * as THREE from 'three';
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize( window.innerWidth / 2, window.innerHeight / 2 );
 
 const threeCanvas = document.querySelector("#threejs-canvas");
 threeCanvas.appendChild( renderer.domElement );
