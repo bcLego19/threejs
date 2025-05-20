3676aab8e8fdd685bf6b7621698f1f7ad0619b41
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2025-05-20 10:13:19 -0700

    Update cube-physics.js

--- scripts/threejs/cube-physics.js
+++ scripts/threejs/cube-physics.js
@@ -6,7 +6,9 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+
+const threeCanvas = document.querySelector("#threejs-canvas");
+threeCanvas.appendChild( renderer.domElement );
 
 // Step 2: Create the Board
 var boardWidth = 10;
